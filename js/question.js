7b239daf5bb102af13b1bf691dfcda9d7409f595
anuvarov <anuvarov0103@gmail.com>
2021-02-05 23:26:57 +0500

    savoller

--- js/question.js
+++ js/question.js
@@ -1,32 +1,52 @@
 const quiz = [
     {
-        q: 'Nechta fasl bor?',
-        options:['2', '3', '4', '5'],
-        answer:2
-    },
-    {
-        q: 'Nechta okean bor?',
-        options:['3', '4', '5', '6'],
-        answer:1
-    },
-    {
-        q: 'Bir yilda nechta oy bor?',
-        options:['21', '13', '14', '12'],
+        q: 'I ______ bus on Mondays.',
+        options:["'m going to work with", "'m going to work by", "go to work with", "go to work by"],
         answer:3
     },
     {
-        q: '54 + 43 = ',
-        options:['97', '99', '87', '88'],
+        q: 'Sorry, but this chair is ______.',
+        options:['me', 'mine', 'my', 'our'],
+        answer:1
+    },
+    {
+        q: "A: 'How old ______?'   B: 'I ______ .'",
+        options:['are you / am 20 years old.', ' have you / have 20 years old', ' are you / am 20 years.', ' do you have / have 20 years.'],
         answer:0
     },
     {
-        q: '6 * 9 = ',
-        options:['48', '56', '63', '62'],
+        q: 'I ______ to the cinema.',
+        options:['not usually go', "don't usually go", "don't go usually", 'do not go usually'],
+        answer:0
+    },
+    {
+        q: 'Where ______ ?',
+        options:['your sister works', 'your sister work', 'does your sister work', 'do your sister work'],
         answer:2
     },
     {
-        q: 'Ismingiz nima? ',
-        options:['Farid', 'Abror', 'Dilshod', 'Ergashali'],
+        q: 'The test is ______ February.',
+        options:['in', 'at', 'on', 'over'],
+        answer:2
+    },
+    {
+        q: 'I eat pasta ______ week.',
+        options:['twice in a', 'twice a', 'one time a', 'once in a'],
+        answer:1
+    },
+    {
+        q: "I don't have ______ free time.",
+        options:['many', 'any', 'a lot', 'some'],
+        answer:2
+    },
+    {
+        q: "A: '_____ to the cinema tomorrow?'",
+        options:['We will go', 'Do we go', '. We go', 'Shall we go'],
+        answer:2
+    },
+    {
+        q: 'We went to the market ______ some vegetables.',
+        options:['to buy', 'for buy', 'for to buy', 'for buying'],
         answer:0
     }
 ]
